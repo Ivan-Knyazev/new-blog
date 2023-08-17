6f265e45c879e0a651221e11644bcaa7ee273137
Ivan-Knyazev <Ivan.Knyazev.2005@yandex.ru>
2023-08-18 00:39:50 +0400

    added front setting and CORS in back

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require(`express`);
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 
 const config = require("./utils/loadEnv") || 3000;
@@ -11,15 +11,24 @@ const postRouter = require("./routers/post");
 
 frontPatch = "http://" + config.frontHost + ":" + config.frontPort;
 app.use(express.json()); // Настройка POST-запроса — JSON
-app.use(cors({ origin: frontPatch })); // Настройка CORS
+// app.use(cors({ origin: frontPatch })); // Настройка CORS
 app.disable("x-powered-by"); // Отключение "ненужных" заголовков [server, via]
 
-// app.use((_req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', '*');
-  
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Alow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     // res.setHeader('Content-Type', 'application/json');
+//     // res.setHeader('Access-Control-Allow-Credentials', true);
 //     next();
 // });
+
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
 
 app.use("/", postRouter);
 
