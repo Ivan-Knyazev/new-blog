f0c26899cdbd973fabd854caee71f1eea1a343db
Ivan-Knyazev <Ivan.Knyazev.2005@yandex.ru>
2023-09-07 00:53:50 +0300

    Updated ENVs, compose

--- backend/index.js
+++ backend/index.js
@@ -6,29 +6,21 @@ const config = require("./utils/loadEnv") || 3000;
 const postRouter = require("./routers/post");
 
 
-// app.use(express.static('public')); // Раздача статических файлов
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public')); // Раздача статических файлов
+app.use(express.urlencoded({ extended: true }));
 
-frontPatch = "http://" + config.frontHost + ":" + config.frontPort;
-app.use(express.json()); // Настройка POST-запроса — JSON
+// frontPatch = "http://" + config.frontHost + ":" + config.frontPort;
 // app.use(cors({ origin: frontPatch })); // Настройка CORS
+
+app.use(express.json()); // Настройка POST-запроса — JSON
 app.disable("x-powered-by"); // Отключение "ненужных" заголовков [server, via]
-
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Alow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     // res.setHeader('Content-Type', 'application/json');
-//     // res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
-
 app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
   });
+
 
 app.use("/", postRouter);
 
